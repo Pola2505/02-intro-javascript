c9b5ecf6e27ec5febee7bcf9261d7ae7b5987089
Sandra Popielarczyk <sandra27popielarczyk@gmail.com>
2023-11-28 21:48:12 -0300

    const let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+
+// Variables y constantes
+
+const nombre = 'Sandra';
+const apellido = 'Popielarczyk';
+
+let valorDado = 5;
+
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+if ( true ) {
+    const nombre = 'Fernando'; // esta permitido usarlo porque es otro scoop
+    console.log(nombre);
+}
