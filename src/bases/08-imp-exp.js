2dbcff47b9a0a3207a058a4b7fe6fad26d2071fb
Sandra Popielarczyk <sandra27popielarczyk@gmail.com>
2023-11-30 23:07:37 -0300

    promesas

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,12 +1,12 @@
 // import { heroes } from './data/heroes';
 // import {heroes} from './data/heroes';
-import { heroes } from './data/heroes';
+import { heroes } from '../data/heroes';
 
-const getHeroeById = (id) => heroes.find( heroe => heroe.id === id);
+export const getHeroeById = (id) => heroes.find( heroe => heroe.id === id);
 
-console.log(getHeroeById(2));
+// console.log(getHeroeById(2));
 
 
-const getHeroesByOwner = (owner) => heroes.filter( heroe => heroe.owner === owner);
+export const getHeroesByOwner = (owner) => heroes.filter( heroe => heroe.owner === owner);
 
-console.log( getHeroesByOwner('DC'));
+// console.log( getHeroesByOwner('DC'));
